9435cb965cf1f9aee164ab2d212495ef6c3d9cfc
Ethan <425734130@qq.com>
2018-02-21 22:11:38 +0800

    完成轮播功能

--- dist/scripts/slider.js
+++ dist/scripts/slider.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -13,18 +13,36 @@ var Slider = function () {
         console.log(options);
         this.$el = options.el;
         this.slides = options.slides;
+        this.index = 0;
         this.render();
+        this.start();
     }
 
     _createClass(Slider, [{
-        key: "render",
+        key: 'render',
         value: function render() {
             this.$el.innerHTML = '<div class="qq-slider-wrap"></div>';
             this.$wrap = this.$el.firstElementChild;
-            this.$wrap.style.width = this.slides.length * 100 + "%";
+            this.$wrap.style.width = this.slides.length * 100 + '%';
             this.$wrap.innerHTML = this.slides.map(function (slide) {
-                return "<div class=\"qq-slider-item\">                <a href=\"" + slide.link + "\">                    <img src=\"" + slide.image + "\">                </a>            </div>";
+                return '<div class="qq-slider-item">                <a href="' + slide.link + '">                    <img src="' + slide.image + '">                </a>            </div>';
             }).join("");
+        }
+    }, {
+        key: 'start',
+        value: function start() {
+            setInterval(this.next.bind(this), 3000);
+        }
+    }, {
+        key: 'next',
+        value: function next() {
+            this.index += 1;
+            if (this.index === this.slides.length) {
+                this.index = 0;
+                this.$wrap.style.transform = 'translate(';
+            }
+            var x = '-' + this.index / this.slides.length * 100 + '%';
+            this.$wrap.style.transform = 'translateX(' + x + ')';
         }
     }]);
 
--- scripts/slider.js
+++ scripts/slider.js
@@ -3,7 +3,9 @@ class Slider{
         console.log(options)
         this.$el = options.el
         this.slides = options.slides
+        this.index = 0
         this.render()
+        this.start()
     }
 
     render(){
@@ -19,5 +21,17 @@ class Slider{
         ).join("")
     }
 
+    start(){
+        setInterval(this.next.bind(this),3000)
+    }
 
-}
+    next(){
+        this.index += 1
+        if(this.index === this.slides.length){
+            this.index = 0
+            this.$wrap.style.transform = 'translateX(0)'
+        }
+        let x = `-${this.index/this.slides.length*100}%`
+        this.$wrap.style.transform = `translateX(${x})`
+    }
+
