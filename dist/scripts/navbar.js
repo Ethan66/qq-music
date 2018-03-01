363025555af5539d1c1d524bcee6bfe9f9d709f4
Ethan <425734130@qq.com>
2018-03-01 12:16:53 +0800

    bar功能完成

---
+++ dist/scripts/navbar.js
@@ -0,0 +1,20 @@
+"use strict";
+
+document.addEventListener("click", function (event) {
+    var target = event.target;
+    if (target.dataset.rola !== "tab") return;
+
+    [].forEach.call(target.parentElement.children, function (tab) {
+        tab.classList.remove("active");
+    });
+    target.classList.add("active");
+
+    var content = document.querySelector("." + target.dataset.view);
+    if (content) {
+        [].forEach.call(content.parentElement.children, function (child) {
+            child.style.display = "none";
+        });
+        content.style.display = "block";
+    }
+});
+//# sourceMappingURL=navbar.js.map
