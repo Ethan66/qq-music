049cf02014b429347f83b8cf364966aa0601912d
Ethan <425734130@qq.com>
2018-03-02 14:52:31 +0800

    排行榜用json进行渲染完成

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 (function () {
 
@@ -10,6 +10,14 @@
         renderSlider(json.data.slider);
         renderRadios(json.data.radioList);
         renderPlayLists(json.data.songList);
+    }
+
+    fetch('json/rank.json').then(function (res) {
+        return res.json();
+    }).then(renderRank);
+
+    function renderRank(json) {
+        renderRankLists(json.data.topList);
     }
 
     function renderSlider(slides) {
@@ -24,13 +32,19 @@
 
     function renderRadios(radios) {
         document.querySelector(".radios .list").innerHTML = radios.map(function (radio) {
-            return "<div class=\"list-item\">\n            <a href=\"\">\n                <div class=\"list-media\">\n                    <img src=\"" + radio.picUrl + "\" />\n                    <span class=\"icon icon-play\"></span>\n                </div>\n                <div class=\"info\">\n                    <h3 class=\"title\">" + radio.Ftitle + "</h3>\n                </div>\n            </a>\n        </div>";
+            return '<div class="list-item">\n            <a href="">\n                <div class="list-media">\n                    <img src="' + radio.picUrl + '" />\n                    <span class="icon icon-play"></span>\n                </div>\n                <div class="info">\n                    <h3 class="title">' + radio.Ftitle + '</h3>\n                </div>\n            </a>\n        </div>';
         }).join("");
     }
 
     function renderPlayLists(playLists) {
         document.querySelector(".playLists .songList").innerHTML = playLists.map(function (playList) {
-            return "<div class=\"list-item\">\n            <a href=\"\">\n                <div class=\"list-media\">\n                    <img src=\"" + playList.picUrl + "\" />\n                    <span class=\"icon icon-play\"></span>\n                </div>\n                <div class=\"info\">\n                    <h3 class=\"title\">" + playList.songListDesc + "</h3>\n                </div>\n            </a>\n        </div>";
+            return '<div class="list-item">\n            <a href="">\n                <div class="list-media">\n                    <img src="' + playList.picUrl + '" />\n                    <span class="icon icon-play"></span>\n                </div>\n                <div class="info">\n                    <h3 class="title">' + playList.songListDesc + '</h3>\n                </div>\n            </a>\n        </div>';
+        }).join("");
+    }
+
+    function renderRankLists(topLists) {
+        document.querySelector(".rank-view .list").innerHTML = topLists.map(function (topList) {
+            return '<li class="list-item">\n                <div class="img">\n                    <a href="">\n                        <img src="' + topList.picUrl + '" />\n                    </a>\n                </div>\n                <div class="text">\n                    <h3>' + topList.topTitle + '</h3>\n                    <p>1 <span>' + topList.songList[0].songname + '</span> - ' + topList.songList[0].singername + '</p>\n                    <p>2 <span>' + topList.songList[1].songname + '</span> - ' + topList.songList[1].singername + '</p>\n                    <p>3 <span>' + topList.songList[2].songname + '</span> - ' + topList.songList[2].singername + '</p>\n                    <i class="arrow"></i>\n                </div>\n            </li>';
         }).join("");
     }
 })();
--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,14 @@
         renderSlider(json.data.slider)
         renderRadios(json.data.radioList)
         renderPlayLists(json.data.songList)
+    }
 
+    fetch('json/rank.json')
+        .then(res => res.json())
+        .then(renderRank)
+
+    function renderRank(json){
+        renderRankLists(json.data.topList)
     }
 
     function renderSlider(slides){
@@ -47,5 +54,23 @@
                 </div>
             </a>
         </div>`).join("")
+    }
+
+    function renderRankLists(topLists){
+        document.querySelector(".rank-view .list").innerHTML = topLists.map(topList =>
+            `<li class="list-item">
+                <div class="img">
+                    <a href="">
+                        <img src="${topList.picUrl}" />
+                    </a>
+                </div>
+                <div class="text">
+                    <h3>${topList.topTitle}</h3>
+                    <p>1 <span>${topList.songList[0].songname}</span> - ${topList.songList[0].singername}</p>
+                    <p>2 <span>${topList.songList[1].songname}</span> - ${topList.songList[1].singername}</p>
+                    <p>3 <span>${topList.songList[2].songname}</span> - ${topList.songList[2].singername}</p>
+                    <i class="arrow"></i>
+                </div>
+            </li>`).join("")
     }
 })()
