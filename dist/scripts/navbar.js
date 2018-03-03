fd085f14dcc0d50944731e37497d7eeae39a605d
Ethan <425734130@qq.com>
2018-03-03 10:13:24 +0800

    modify

--- dist/scripts/navbar.js
+++ dist/scripts/navbar.js
@@ -1,23 +1,28 @@
 "use strict";
 
-document.addEventListener("click", function (event) {
-    var target = event.target;
-    if (target.dataset.rola !== "tab") return;
+(function () {
+    var rankViewClick = 0;
+    document.addEventListener("click", function (event) {
+        var target = event.target;
+        if (target.dataset.rola !== "tab") return;
 
-    [].forEach.call(target.parentElement.children, function (tab) {
-        tab.classList.remove("active");
-    });
-    target.classList.add("active");
-
-    var content = document.querySelector("." + target.dataset.view);
-    if (content) {
-        [].forEach.call(content.parentElement.children, function (child) {
-            child.style.display = "none";
+        [].forEach.call(target.parentElement.children, function (tab) {
+            tab.classList.remove("active");
         });
-        content.style.display = "block";
-        if (content.classList.contains("rank-view")) {
-            lazyload(document.querySelectorAll(".lazyload"));
+        target.classList.add("active");
+
+        var content = document.querySelector("." + target.dataset.view);
+        if (content) {
+            [].forEach.call(content.parentElement.children, function (child) {
+                child.style.display = "none";
+            });
+            content.style.display = "block";
+            if (content.classList.contains("rank-view") && rankViewClick == 0) {
+                console.log(1);
+                lazyload(document.querySelectorAll(".lazyload"));
+                rankViewClick++;
+            }
         }
-    }
-});
+    });
+})();
 //# sourceMappingURL=navbar.js.map
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,21 +1,26 @@
-document.addEventListener("click",function(event){
-    let target = event.target
-    if(target.dataset.rola !== "tab") return
+(function(){
+    let rankViewClick = 0
+    document.addEventListener("click",function(event){
+        let target = event.target
+        if(target.dataset.rola !== "tab") return
 
-    [].forEach.call(target.parentElement.children, tab => {
-        tab.classList.remove("active")
-    })
-    target.classList.add("active")
-
-    let content = document.querySelector(`.${target.dataset.view}`)
-    if(content){
-        [].forEach.call(content.parentElement.children, child => {
-            child.style.display = "none"
+        [].forEach.call(target.parentElement.children, tab => {
+            tab.classList.remove("active")
         })
-        content.style.display = "block"
-        if(content.classList.contains("rank-view")){
-            lazyload(document.querySelectorAll(".lazyload"))
-        }
-    }
+        target.classList.add("active")
 
-})
+        let content = document.querySelector(`.${target.dataset.view}`)
+        if(content){
+            [].forEach.call(content.parentElement.children, child => {
+                child.style.display = "none"
+            })
+            content.style.display = "block"
+            if(content.classList.contains("rank-view") && rankViewClick == 0 ){
+                console.log(1)
+                lazyload(document.querySelectorAll(".lazyload"))
+                rankViewClick++
+            }
+        }
+
+    })
+})()
