db8d43bd682b19eade106b4df5e54929b0d7a879
Ethan <425734130@qq.com>
2018-03-04 20:41:14 +0800

    搜索页面结果显示歌手信息完成

--- dist/scripts/search.js
+++ dist/scripts/search.js
@@ -11,6 +11,7 @@ var Search = function () {
         this.$el = el;
         this.$input = this.$el.querySelector(".search");
         this.$songs = this.$el.querySelector(".search-content ul");
+        this.$singer = this.$el.querySelector(".search-content .singer");
         this.$input.addEventListener("keyup", this.onKeyUp.bind(this));
         this.keyword = '';
         this.page = 1;
@@ -23,8 +24,12 @@ var Search = function () {
         value: function onKeyUp(event) {
             var keyword = event.target.value.trim();
             console.log(keyword);
-            if (keyword == '') return this.$songs.innerHTML = "";
+            if (keyword == '') {
+                this.$songs.innerHTML = "";
+                this.$singer.innerHTML = '';
+            }
             if (event.key !== "Enter") return;
+            this.searchSinger(keyword);
             this.search(keyword);
         }
     }, {
@@ -42,8 +47,28 @@ var Search = function () {
             });
         }
     }, {
+        key: "searchSinger",
+        value: function searchSinger(keyword) {
+            var _this2 = this;
+
+            fetch("http://localhost:4000/search?keyword=" + keyword + "&page=" + this.page).then(function (res) {
+                return res.json();
+            }).then(function (json) {
+                return json.data.zhida;
+            }).then(function (zhida) {
+                return _this2.appendSinger(zhida);
+            });
+        }
+    }, {
+        key: "appendSinger",
+        value: function appendSinger(zhida) {
+            var singerHtml = "\n        <div class=\"img\">\n                        <img src=\"https://y.gtimg.cn/music/photo_new/T001R68x68M000000aHmbL2aPXWH.jpg?max_age=2592000\" />\n                    </div>\n                    <div class=\"text\">\n                        <h4>" + zhida.singername + "</h4>\n                        <p>\u5355\u66F2: <span>" + zhida.songnum + "</span> \u4E13\u8F91: <i>" + zhida.albumnum + "</i></p>\n                    </div>";
+
+            this.$singer.innerHTML = singerHtml;
+        }
+    }, {
         key: "append",
-        value: function append(songs) {
+        value: function append(songs, singer) {
             var html = songs.map(function (song) {
                 return "<li>\n                <div class=\"img\">\n                    <i class=\"icon\"></i>\n                </div>\n                <div class=\"text\">\n                    <h4>" + song.songname + "</h4>\n                    <p>" + song.singer.map(function (singer) {
                     return singer.name;
--- scripts/search.js
+++ scripts/search.js
@@ -3,6 +3,7 @@ class Search{
         this.$el = el
         this.$input = this.$el.querySelector(".search")
         this.$songs = this.$el.querySelector(".search-content ul")
+        this.$singer = this.$el.querySelector(".search-content .singer")
         this.$input.addEventListener("keyup",this.onKeyUp.bind(this))
         this.keyword = ''
         this.page = 1
@@ -13,8 +14,12 @@ class Search{
     onKeyUp(event){
         let keyword = event.target.value.trim()
         console.log(keyword)
-        if(keyword == '') return this.$songs.innerHTML = ""
+        if(keyword == '') {
+            this.$songs.innerHTML = ""
+            this.$singer.innerHTML = ''
+        }
         if(event.key !== "Enter") return
+        this.searchSinger(keyword)
         this.search(keyword)
     }
 
@@ -26,7 +31,27 @@ class Search{
             .then(songs => this.append(songs))
     }
 
-    append(songs){
+    searchSinger(keyword){
+        fetch(`http://localhost:4000/search?keyword=${keyword}&page=${this.page}`)
+            .then(res => res.json())
+            .then(json => json.data.zhida)
+            .then(zhida => this.appendSinger(zhida))
+    }
+
+    appendSinger(zhida){
+        let singerHtml = `
+        <div class="img">
+                        <img src="https://y.gtimg.cn/music/photo_new/T001R68x68M000000aHmbL2aPXWH.jpg?max_age=2592000" />
+                    </div>
+                    <div class="text">
+                        <h4>${zhida.singername}</h4>
+                        <p>单曲: <span>${zhida.songnum}</span> 专辑: <i>${zhida.albumnum}</i></p>
+                    </div>`
+
+        this.$singer.innerHTML = singerHtml
+    }
+
+    append(songs, singer){
         let html = songs.map(song =>`<li>
                 <div class="img">
                     <i class="icon"></i>
